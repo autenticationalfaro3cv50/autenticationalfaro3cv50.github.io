ca25f30eb16b089b3032d547d3d6ad30bda42271
autenticationalfaro3cv50 <84985496+autenticationalfaro3cv50@users.noreply.github.com>
2021-05-28 13:12:38 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAeREWCetXrW4roqkbb8g5txW8aZyLk_8U",
+    authDomain: "autenticationalfaro3cv50-dfe13.firebaseapp.com",
+    projectId: "autenticationalfaro3cv50-dfe13",
+    storageBucket: "autenticationalfaro3cv50-dfe13.appspot.com",
+    messagingSenderId: "147335719485",
+    appId: "1:147335719485:web:74ebacd8f43087773b1854"
+    });
